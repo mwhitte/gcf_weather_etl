e9ae1b1576a499c1b52809ac31eedd3e8cbd6d47
Mariah Whitted <mwhitte@iu.edu>
2024-02-11 12:36:11 +0000

    End of v3 - Helper function to format console output

--- index.js
+++ index.js
@@ -19,9 +19,19 @@ exports.readObservation = (file, context) => {
    .pipe(csv())
    .on('data', (row) => {
     // Log row data 
-    console.log(row)
+    // console.log(row)
+    printDict(row);
    })
    .on('end', () => {
     console.log('End!');
    })
+}
+
+// HELPER FUNCTIONS 
+
+function printDict(row) {
+    for (let key in row) {
+        console.log(key + ' : ' + row[key]);
+        console.log('${key} : ${row[key]}');
+    }
 }
