21b200e030995c5303ff2fac9562715271b0e239
Mariah Whitted <mwhitte@iu.edu>
2024-02-11 12:24:48 +0000

    End of v2 - logging data to the console

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+const {Storage} = require('@google-cloud/storage');
+const csv = require('csv-parser');
+
 exports.readObservation = (file, context) => {
-    console.log(`  Event: ${context.eventId}`);
-    console.log(`  Event Type: ${context.eventType}`);
-    console.log(`  Bucket: ${file.bucket}`);
-    console.log(`  File: ${file.name}`);
+   // console.log(`  Event: ${context.eventId}`);
+   // console.log(`  Event Type: ${context.eventType}`);
+   // console.log(`  Bucket: ${file.bucket}`);
+   // console.log(`  File: ${file.name}`);
+
+   const gcs = new Storage();
+
+   const dataFile = gcs.bucket(file.bucket).file(file.name);
+
+   dataFile.createReadStream()
+   .on('error', () => {
+    //Handle an error 
+    console.error(error);
+   })
+   .pipe(csv())
+   .on('data', (row) => {
+    // Log row data 
+    console.log(row)
+   })
+   .on('end', () => {
+    console.log('End!');
+   })
 }
